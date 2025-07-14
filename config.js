6bff5774d8319da329933648678c2738757bcd5d
OdhisGaga <nyolalmakaburi@gmail.com>
2025-07-14 16:37:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -41,15 +41,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255713XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254112291443",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DAVINCS-TCEH",
+OWNER_NAME: process.env.OWNER_NAME || "Gaga",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© CREDITS TO GAGA*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/xbrltv.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *DAVINCS-XMD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> xgaga",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255759637644",
+DEV: process.env.DEV || "254112291443",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
